78aa6e9135e82824f320c582bf1840c83315dec9
Brendan McVeigh <mcveigh.brendan@gmail.com>
2018-05-14 01:26:18 -0400

    add a version of bipartite clustering algorithm that works on sparse matricies

--- src/clustering_functions.jl
+++ src/clustering_functions.jl
@@ -143,6 +143,91 @@ function bipartite_cluster(weightArray::Array{<:AbstractFloat, 2}, threshold::Ab
     return rowLabels, colLabels, maxLabel
 end
 
+
+function bipartite_cluster{A <: SparseMatrixCSC}(linkArray::A)
+    transposeArray = linkArray'
+    n, m = size(linkArray)
+    rowLabels = zeros(Int64, n)
+    colLabels = zeros(Int64, m)
+    rowUnassigned = trues(n)
+    colUnassigned = trues(m)
+    rowQ = Queue(Int64)
+    colQ = Queue(Int64)
+
+    rows = rowvals(linkArray)
+    cols = rowvals(transposeArray)
+    
+    maxLabel = 0
+    nextCol = 1
+    while nextCol > 0 && nextCol <= m
+        
+        ##Mark column
+        colUnassigned[nextCol] = false
+        
+        ##Seach column for links
+        for ii in nzrange(linkArray, nextCol)#1:n
+            #if rowUnassigned[ii] && linkArray[ii, nextCol]
+            #    enqueue!(rowQ, ii)
+            #    rowUnassigned[ii] = false
+            #end
+            if rowUnassigned[rows[ii]] #&& linkArray[rows[ii], nextCol]
+                enqueue!(rowQ, rows[ii])
+                rowUnassigned[rows[ii]] = false
+            end
+        end
+
+        ##Only assign column if row link found (otherwise column links to nothing)
+        if length(rowQ) > 0
+            maxLabel += 1
+            colLabels[nextCol] = maxLabel
+        end
+
+        ##Continue adding to cluster until Queues are empty
+        while length(rowQ) > 0 || length(colQ) > 0
+
+            ##Dequeue row and seach row for links to unassigned columns
+            if length(rowQ) > 0
+                row = dequeue!(rowQ)
+                rowLabels[row] = maxLabel
+                #for jj in 1:m
+                #    if colUnassigned[jj] && linkArray[row, jj]
+                #        enqueue!(colQ, jj)
+                #        colUnassigned[jj] = false
+                #    end
+                #end
+                for jj in nzrange(transposeArray, row)
+                    if colUnassigned[cols[jj]] #&& linkArray[row, cols[jj]]
+                        enqueue!(colQ, cols[jj])
+                        colUnassigned[cols[jj]] = false
+                    end
+                end
+            end
+
+            ##Dequeue column and seach column for links to unassigned rowss
+            if length(colQ) > 0
+                col = dequeue!(colQ)
+                colLabels[col] = maxLabel
+                #for ii in 1:n
+                #    if rowUnassigned[ii] && linkArray[ii, col]
+                #        enqueue!(rowQ, ii)
+                #        rowUnassigned[ii] = false
+                #    end
+                #end
+                for ii in nzrange(linkArray, col)#1:n
+                    if rowUnassigned[rows[ii]] #&& linkArray[rows[ii], nextCol]
+                        enqueue!(rowQ, rows[ii])
+                        rowUnassigned[rows[ii]] = false
+                    end
+                end
+            end
+        end
+        
+        ##Queue's are empty so move to next column
+        nextCol = findnext(colUnassigned, nextCol + 1)
+    end
+    return rowLabels, colLabels, maxLabel
+end
+
 function sparseblock_idxlims{A <: SparseMatrixCSC}(linkArray::A)
     n, m = size(linkArray)
     col2rowstart = fill(n + 1, m)
