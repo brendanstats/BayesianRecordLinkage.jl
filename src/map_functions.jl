83fe967be90a53b65c93d34bc4429d2983ade0d6
Brendan McVeigh <mcveigh.brendan@gmail.com>
2018-06-16 18:12:53 -0400

    tight convergence criterion for auction algorithms, change max_C_cluster2 cost matrix to sparsematrix

--- src/map_functions.jl
+++ src/map_functions.jl
@@ -83,7 +83,7 @@ function map_solver_cluster{G <: AbstractFloat, T <: Real}(pM0::Array{G, 1},
     ##Modes are found using pseudo counts of 1 - αᵢ
     pseudoM = priorM - ones(T, length(priorM))
     pseudoU = priorU - ones(T, length(priorU))
-    currmrows, currmcols = max_C_cluster(pM0, pU0, compsum, penalty)
+    currmrows, currmcols = max_C_cluster(pM0, pU0, compsum, penalty, verbose = verbose)
     pM = copy(pM0)
     pU = copy(pU0)
     iter = 0
